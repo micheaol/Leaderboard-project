07413526dfaadb7e7011dcc5c038b0e57ba3fa92
micheaol <micheaol80@gmail.com>
2021-11-09 21:42:13 +0100

    feat: render the footer in the DOM

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+
+const footer = `
+<footer><p>© 2021 Leaderboard</p></footer>
+`;
+
+const renderFooter = () => {
+    body.insertAdjacentHTML('beforeend', footer);
+   
+  };
+  
+  export default renderFooter;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
 import renderHome from './home.js';
+import renderFooter from './footer.js';
 
+renderFooter();
 renderHome();
