53915d2b76734f3e5ee735b2bf24bba6cf8fd79e
micheaol <micheaol80@gmail.com>
2021-11-10 18:46:01 +0100

    add logic to fetch data from the API

--- src/formdata.js
+++ src/formdata.js
@@ -1,7 +1,10 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import renderHome from './home.js';
 
 renderHome();
 const form = document.querySelector('#form-id');
+const refresh = document.querySelector('#refresh-score-btn');
 
 function getForm(e) {
   e.preventDefault();
@@ -9,28 +12,45 @@ function getForm(e) {
   const score = document.querySelector('#score-field');
 
   //fetch to POST the data:
-  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/TAndiRIq5l6QgrYO74Z7/scores', {
+  fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/TAndiRIq5l6QgrYO74Z7/scores',
+    {
       method: 'POST',
       body: JSON.stringify({
-          user: name.value,
-          score: score.value,
+        user: name.value,
+        score: score.value,
       }),
       headers: {
-          "Content-Type": "application/json; charset=UTF-8"
-      }
-  }).then(function (res) {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    }
+  )
+    .then(function (res) {
       return res.json();
-  }).then(function (data) {
-      console.log(data)
-  })
+    })
+    .then(function (data) {
+      const message = document.querySelector('#message');
+      message.innerHTML = data.result;
+      message.style.backgroundColor = 'green';
+    });
 }
 
+async function fetchUser() {
+    const user = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/TAndiRIq5l6QgrYO74Z7/scores');
+
+    const data = await user.json();
+    console.log(data)
+
+
+}
+
+
+
+refresh.addEventListener('click', fetchUser);
 form.addEventListener('submit', getForm);
 
 export default getForm;
 
-
 //to create new score for the game:
 //POST : https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/TAndiRIq5l6QgrYO74Z7/scores
 //GET : https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/TAndiRIq5l6QgrYO74Z7/scores
-
--- src/home.js
+++ src/home.js
@@ -23,6 +23,7 @@ const setScore = `
 <div id="set-score">
 <form id="form-id">
     <h3>Add your score</h3>
+    <p id=message></p>
     <input type="text" placeholder="Your name" id="name-field">
     <input type="text" placeholder="Your score" id="score-field">
     <button type="submit" value="Submit" id="submit-score-btn">Submit</button>
