c812d05091402d88958130af3611523c1e682d59
micheaol <micheaol80@gmail.com>
2021-11-10 22:27:55 +0100

    fix linter error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './style.css';
-// import renderHome from './home.js';
 import renderFooter from './footer.js';
 import getForm from './formdata.js';
 
 getForm();
 renderFooter();
-// renderHome();
+
